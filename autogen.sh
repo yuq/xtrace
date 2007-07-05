8ab46a6dae416d637ffbfa4daa1060198313a5ca
Bernhard R. Link <brlink@debian.org>
2007-07-05 15:38:05 +0000

    add --chdir option

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,19 @@
 #!/bin/sh
+set -e
+
+configindir="."
+if [ "x$1" = "x--chdir" ] ; then
+	shift
+	configindir="$1"
+	shift
+fi
+
 aclocal
 autoheader
 automake -a -c
 autoconf
-./configure --enable-maintainer-mode CFLAGS="-Wall -O2 -g -Wmissing-prototypes -Wstrict-prototypes -W -DSTUPIDCC=1"
+
+curdir="$(pwd)"
+mkdir -p -- "$configindir"
+cd "$configindir" || exit 1
+"$curdir"/configure --enable-maintainer-mode CFLAGS="-Wall -O2 -g -Wmissing-prototypes -Wstrict-prototypes -W -DSTUPIDCC=1"
